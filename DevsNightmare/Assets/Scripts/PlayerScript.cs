4a69cab71c8503755069dfe8c306c7cb3e0c1f90
FABRICIO DE SOUZA GENTILE - STUDENT <G00363827@gmit.ie>
2020-02-25 11:56:51 +0000

    Player movement

--- DevsNightmare/Assets/Scripts/PlayerScript.cs
+++ DevsNightmare/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Speed of the spaceship
+    public Vector2 speed = new Vector2(10, 10);
+
+    //Movement of the spaceship
+
+    private Rigidbody2D rb;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        Move();
+
     }
+
+    private void Move()
+    {
+        //Positions
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        //Move the game object
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(inputX * speed.x, inputY * speed.y); ;
+    }
+
 }
