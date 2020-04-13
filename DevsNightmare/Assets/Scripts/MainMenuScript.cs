4fa091d70723ab543db2ddefa9907262d183e537
Fabricio Gentile <g00363827@gmit.ie>
2020-04-13 01:30:26 +0100

    pause menu

---
+++ DevsNightmare/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void HTP()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting...");
+    }
+
+}
